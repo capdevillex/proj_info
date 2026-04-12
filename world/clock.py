b26074c6e07b3ce45f08c8cedf89e25764695bb5
capdevillex <capdeville.x@gmail.com>
2026-04-13 00:00:42 +0200

    ajout de la règle mouvement en fin de tour

--- world/clock.py
+++ world/clock.py
@@ -5,6 +5,10 @@ class TurnManager:
     def next_turn(self, game_map):
         self.current_turn += 1
         print(f"\nET attention pour le changement de tour !\nTour N°{self.current_turn} !")
+
+        for tile in game_map.tiles.values():
+            for unit in tile.units:
+                unit.reset_movement()
         
         # C'est ici (plus tard) qu''on pourras dire :
         # - Redonner des points de mouvement aux unités
